32b4761523a691589184d73afb675bac8f236539
jayden14141 <sd21099@bristol.ac.uk>
2022-04-16 15:38:15 +0100

    Restructured project structure

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -6,8 +6,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.Nonnull;
 
-
-import com.google.common.collect.ImmutableSet;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.*;
 
@@ -19,13 +17,20 @@ public class MyAi implements Ai {
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
 		// returns a random move, replace with your own implementation
-//		var moves = board.getAvailableMoves().asList();
-//		return moves.get(new Random().nextInt(moves.size()));
-
-		BestMove bestMove = new BestMove(board);
-		return bestMove.getBestMove();
-//		Map<Move, Integer> scoreMap = getScoreMap(board, board.getAvailableMoves());
-//		return bestMove(scoreMap);
+		var moves = board.getAvailableMoves().asList();
+		return moves.get(new Random().nextInt(moves.size()));
+//
+//		BestMove bestMove = new BestMove(board);
+//		return bestMove.getBestMove();
 	}
+
+	// Helper function to return the best move among the moves derived from the score
+	// Compares score value from a map and returns the corresponding key(Move)
+	private Move bestMove(Board board) {
+		ScoreMap sc = new ScoreMap(board);
+		//TODO make an algorithm to return the move which has the highest score
+		return null;
+	}
+
 
 }
--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/ScoreMap.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/ScoreMap.java
@@ -3,32 +3,41 @@ package uk.ac.bris.cs.scotlandyard.ui.ai;
 import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public final class BestMove {
+// A class to map the certain move with its assigned score
+// The elements are marked as private, though they can be extracted with getters
+public final class ScoreMap {
 
-    Map<Move, Integer> scoreMap;
-    Move bestMove;
+    List<Score> scoreMap = new ArrayList<>();
+    final Board board;
 
-    public BestMove(Board board) {
-        scoreMap = getScoreMap(board, board.getAvailableMoves());
-        bestMove = bestMove(scoreMap);
+    public ScoreMap(Board board) {
+        this.board = board;
+        this.scoreMap = getScoreMaps(board);
     }
 
-    // Getter to return the best Move
-    public Move getBestMove() {
-        return this.bestMove;
+    private final class Score {
+
+        private Move move;
+        private int score;
+
+        private Score(final Move move, final int score) {
+            this.move = move;
+            this.score = score;
+        }
     }
 
     // Getter to return detectives in the board
-    public List<Piece> getDetectives(Board board) {
+    private List<Piece> getDetectives(Board board) {
         List<Piece> detectives = board.getPlayers().asList();
         detectives.removeIf(p -> p.isMrX());
         return detectives;
     }
 
     // Getter to return the node value from a Move
-    public Integer getDestination(Move move) {
+    private Integer getDestination(Move move) {
         return move.accept(new Move.Visitor<>() {
             @Override
             public Integer visit(Move.SingleMove move) {
@@ -42,25 +51,17 @@ public final class BestMove {
         });
     }
 
-    // Helper function to return the best move among the moves derived from the score
-    // Compares score value from a map and returns the corresponding key(Move)
-    private Move bestMove(Map<Move,Integer> scoreMap) {
-        return Collections.max(scoreMap.entrySet(), Map.Entry.comparingByValue())
-                .getKey();
-    }
-
     // Helper function that returns Enum Detective by its piece
     private Piece.Detective getDetectiveByPiece(Piece piece) {
         String colour = piece.webColour();
-        Piece.Detective detective;
-        switch(colour) {
-            case "#f00" : detective = Piece.Detective.RED; break;
-            case "#0f0" : detective = Piece.Detective.GREEN; break;
-            case "#00f" : detective = Piece.Detective.BLUE; break;
-            case "#fff" : detective = Piece.Detective.WHITE; break;
-            case "#ff0" : detective = Piece.Detective.YELLOW; break;
-            default : throw new IllegalArgumentException("No detectives matched");
-        }
+        Piece.Detective detective = switch (colour) {
+            case "#f00" -> Piece.Detective.RED;
+            case "#0f0" -> Piece.Detective.GREEN;
+            case "#00f" -> Piece.Detective.BLUE;
+            case "#fff" -> Piece.Detective.WHITE;
+            case "#ff0" -> Piece.Detective.YELLOW;
+            default -> throw new IllegalArgumentException("No detectives matched");
+        };
         return detective;
     }
 
@@ -83,13 +84,13 @@ public final class BestMove {
     }
 
     // Reference 'https://boardgamegeek.com/thread/102272/scotland-yard-basic-strategy' for basic strategy for mrX
-    // Helper function to map scores with moves
-    // Used Map<Move, Int> as a return value; Indicating Move(Move) and score(Int) respectively
-    private Map<Move, Integer> getScoreMap(Board board, ImmutableSet<Move> mv) {
-        Map<Move, Integer> scoreMap = new HashMap<>();
+    // Helper function to add score class to scoreMap
+
+    private List<Score> getScoreMaps(Board board) {
+        ImmutableSet<Move> mv = board.getAvailableMoves();
         for(Move m : mv) {
-            scoreMap.put(m, score(board, m));
-            System.out.println("By move m:" + m + "Score: " + score(board,m));
+            scoreMap.add(score(m));
+            System.out.println("By move m:" + m + "Score: " + score(m));
         }
         return scoreMap;
     }
@@ -100,12 +101,15 @@ public final class BestMove {
     // 1. Places which are near detectives get a low score
     // 1-1. If mrX's next move is the place where detectives can reach by one move, score -50 is allocated
     // 2. Place where has various choices of transportation gets high score
-    private Integer score(Board board, Move m) {
+    // Main function to evaluate a move by assigning score
+    private Score score(Move m) {
         int n = 0;
         List<Integer> potential = detectivePotential(board);
         for(int i :potential) {
-            if(getDestination(m) == i) return -50;
+            if(getDestination(m) == i) n -= 50;
         }
-        return n;
+        return new Score(m, n);
     }
+
+
 }
